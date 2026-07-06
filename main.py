d308a5b122bdb7097568aa8b72a415ee68a79776
tubusor <160207643+Tobitza@users.noreply.github.com>
2026-07-06 14:24:21 +0300

    Update FPS

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             pass
+
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
