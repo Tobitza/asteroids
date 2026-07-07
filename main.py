186a72506c166f8b77e5afb8f42904b8802ced42
tubusor <160207643+Tobitza@users.noreply.github.com>
2026-07-08 00:45:59 +0300

    Added player character

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,6 +10,7 @@ def main():
 
     pygame.init()
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0.0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,12 +20,13 @@ def main():
 
         for event in pygame.event.get():
             pass
-
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
-
+        
 
 if __name__ == "__main__":
     main()
